885990e8812bba8891b170794b00e23a7dd5240c
gmickus <g.mickus@ba.lt>
2024-08-14 13:01:29 +0300

    Some changes

--- src/v2/formatters/using/UsingFormatter.ts
+++ src/v2/formatters/using/UsingFormatter.ts
@@ -4,16 +4,31 @@ import { IFormatter } from "../../formatterFramework/IFormatter";
 import { CodeEdit } from "../../model/CodeEdit";
 import { FullText } from "../../model/FullText";
 import { AFormatter } from "../AFormatter";
+import { IConfigurationManager } from "../../../utils/IConfigurationManager";
+import { FormatterHelper } from "../../formatterFramework/FormatterHelper";
+import { UsingSettings } from "./UsingSettings";
 
 @RegisterFormatter
 export class UsingFormatter extends AFormatter implements IFormatter {
+    private readonly settings: UsingSettings;
+
+    public constructor(configurationManager: IConfigurationManager) {
+        super(configurationManager);
+        this.settings = new UsingSettings(configurationManager);
+    }
+
     match(node: Readonly<SyntaxNode>): boolean {
-        throw new Error("Method not implemented.");
+        if (node.type === "USING" || node.type === "using") {
+            return true;
+        }
+        return false;
     }
     parse(
         node: Readonly<SyntaxNode>,
         fullText: Readonly<FullText>
     ): CodeEdit | CodeEdit[] | undefined {
-        throw new Error("Method not implemented.");
+        const text = FormatterHelper.getCurrentText(node, fullText);
+        console.log(text);
+        return this.getCodeEdit(node, text, text);
     }
 }
