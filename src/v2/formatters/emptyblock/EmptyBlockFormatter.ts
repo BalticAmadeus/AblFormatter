241bc237f72b8228db212c3906c6ec09cbdb8a3f
gmickus <g.mickus@ba.lt>
2024-08-20 10:34:28 +0300

    Partial implementation

--- src/v2/formatters/emptyblock/EmptyBlockFormatter.ts
+++ src/v2/formatters/emptyblock/EmptyBlockFormatter.ts
@@ -6,6 +6,8 @@ import { FullText } from "../../model/FullText";
 import { AFormatter } from "../AFormatter";
 import { EmptyBlockSettings } from "./EmptyBlockSettings";
 import { IConfigurationManager } from "../../../utils/IConfigurationManager";
+import { SyntaxNodeType } from "../../../model/SyntaxNodeType";
+import { FormatterHelper } from "../../formatterFramework/FormatterHelper";
 
 @RegisterFormatter
 export class EmptyBlockFormatter extends AFormatter implements IFormatter {
@@ -18,14 +20,78 @@ export class EmptyBlockFormatter extends AFormatter implements IFormatter {
     }
 
     match(node: Readonly<SyntaxNode>): boolean {
-        let found: boolean = false;
+        if (node.type !== SyntaxNodeType.DoBlock) {
+            return false;
+        }
 
-        return found;
+        for (let child of node.children) {
+            if (
+                child.type === SyntaxNodeType.Body ||
+                child.type === SyntaxNodeType.CaseBody ||
+                child.type === SyntaxNodeType.ClassBody
+            ) {
+                return false;
+            }
+        }
+        return true;
     }
     parse(
         node: Readonly<SyntaxNode>,
         fullText: Readonly<FullText>
     ): CodeEdit | CodeEdit[] | undefined {
-        throw new Error("Method not implemented.");
+        const text = FormatterHelper.getCurrentText(node, fullText);
+        const statementIndentation =
+            FormatterHelper.getActualStatementIndentation(node, fullText);
+
+        const lastLine = FormatterHelper.getCurrentText(node, fullText)
+            .split(fullText.eolDelimiter)
+            .slice(-1)[0];
+
+        const endNode = node.children.find(
+            (child) => child.type === SyntaxNodeType.EndKeyword
+        );
+
+        if (endNode !== undefined) {
+            const endRowDelta =
+                statementIndentation -
+                FormatterHelper.getActualTextIndentation(lastLine, fullText);
+            console.log("ind: " + statementIndentation);
+            console.log(
+                "endInd: " +
+                    FormatterHelper.getActualTextIndentation(lastLine, fullText)
+            );
+            console.log("indLastLine: " + lastLine);
+            console.log(
+                "indFirstLine: " +
+                    FormatterHelper.getCurrentText(node, fullText)
+            );
+            console.log("indchildren: " + node.childCount);
+            node.children.forEach((child) => {
+                console.log(
+                    "indchild: " +
+                        FormatterHelper.getCurrentText(child, fullText)
+                );
+            });
+
+            // if (endRowDelta !== 0) {
+            //     indentationEdits.push({
+            //         line: parent.endPosition.row - parent.startPosition.row,
+            //         lineChangeDelta: endRowDelta,
+            //     });
+            // }
+        }
+
+        return undefined;
     }
+
+    private applyIndentationEdit(
+        code: string,
+        indentationEdit: IndentationEdits,
+        fullText: FullText
+    ) {}
+}
+
+interface IndentationEdits {
+    line: number;
+    lineChangeDelta: number;
 }
